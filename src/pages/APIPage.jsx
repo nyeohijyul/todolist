c8dcb4dd0663efe69134a8a318c200a1b24733df
nyeohijyul <nyeohijyul@gmail.com>
2026-05-19 04:08:58 +0900

    API 요청 성공

--- src/pages/APIPage.jsx
+++ src/pages/APIPage.jsx
@@ -8,10 +8,12 @@ function ApiPage() {
         const tryApiRequest = async () => {
             try {
                 // 일부러 존재하지 않는 API 주소로 요청해서 실패 흐름을 확인합니다.
-                await axios.get("https://example.invalid/todos");
+                // await axios.get("https://example.invalid/todos");
                 // const response = await axios.get("https://test-project-eight-sage.vercel.app/api/260519");
                 // setData(response.data);
                 // console.log("API 응답 데이터: ", response.data);
+                const response = await axios.get("https://jsonplaceholder.typicode.com/posts?_limit=5");
+                setData(response.data);
             } catch {
                 // 요청이 실패하면 실패 상태를 true로 바꿔 화면에 "실패"를 표시합니다.
                 setIsError(true);
@@ -30,6 +32,11 @@ function ApiPage() {
     }
     // API 성공 시에 뜰 문구
     // 이번 실습에서는 가짜 API 주소이므로 성공 불가함!!
-    return <section>성공 {data.message}</section>;
+    return <section>성공 {data.map((item) => (
+        <dl>
+            <dt>{item.title}</dt>
+            <dd>{item.body}</dd>
+        </dl>
+    ))}</section>;
 }
 export default ApiPage;
